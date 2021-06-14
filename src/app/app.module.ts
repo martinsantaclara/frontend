86377b1343474b467e0889116bbac62dff39387d
Martin <martinsantaclara@gmail.com>
2021-06-14 19:49:58 -0300

    feat register1 close #7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { Login2Component } from './components/login2/login2.component';
 import { RegisterComponent } from './components/register/register.component';
+import { Register1Component } from './components/register1/register1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     Login2Component,
-    RegisterComponent
+    RegisterComponent,
+    Register1Component
   ],
   imports: [
     BrowserModule,
