42b79dd5eb6f7b2595008856e14456fe03ac69ae
Martin <martinsantaclara@gmail.com>
2021-06-14 17:01:00 -0300

    feat login2 close #4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { Login2Component } from './components/login2/login2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    Login2Component
   ],
   imports: [
     BrowserModule,
