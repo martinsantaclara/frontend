30263f29e9386238ecfe6bd83b116c363dc072c4
Martin <martinsantaclara@gmail.com>
2021-06-15 10:38:52 -0300

    feat register1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './conmponents/login/login.component';
+import { Register1Component } from './conmponents/register1/register1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    Register1Component
   ],
   imports: [
     BrowserModule,
